fb63da066d0c8e145f466d8e2f176564c0d0f712
David Nishikawa <nishikawadavid@gmail.com>
2022-08-20 04:47:15 -0700

    pretty tired now..

--- nnnhh.sh
+++ nnnhh.sh
@@ -11,6 +11,30 @@ git status
 git commit -m 'Initial commit.'
 git log
 npm init
+yarn start
+npm i yarn
+npm uninstall yarn
+npm i -g yarn
+npm uninstall -g yarn
+git commit -m 'npm'
+npm i express
+npm install netlify-cli -g
+chmod u+x nnnhh.sh        
+
+???
+git commit -m 'nnnnnnghhhh'
+Set-Location C:\path\to\myrepo.git\
+sh nnnhh
+git add A
+git committ -m 'nnnnnnghhhh'
+git log
+Set-Location C:\path\to\myrepo.git\
+Set-Location C:\projects\the_code 
+git pull
+
+
+
+
 
 #                       https://github.com/orgs/Feeling-Resonant-n-Shit/repositories/myFirstRepo.git
 # git remote add origin https://github.com/harishrajora805                          /myFirstRepo.git
@@ -18,3 +42,41 @@ npm init
   git remote add origin https://github.com/orgs/Feeling-Resonant-n-Shit/repositories/myFirstRepo.git
 
 # /* nnnhh */
+
+'nnnnnnghhhh'
+
+'ask the world, and the world giveth you'
+
+'fuckin weird'
+
+
+
+chmod u+x nnnhh.sh
+
+
+
+'*madess*'
+
+
+
+'what next'
+'wet wot?'
+
+'rofl'
+'drink some water'
+'~chokes~'
+
+'what now though?'
+
+''
+''
+
+'what do we do?'
+'we could ask for instructions'
+
+"good. we'll do that"
+'where do instructions come from'
+'idk'
+'look around for questions'
+
+
