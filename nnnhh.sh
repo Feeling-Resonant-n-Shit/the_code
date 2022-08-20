48f54f990759ef8fe56f178e313ff3783a50e5e4
David Nishikawa <nishikawadavid@gmail.com>
2022-08-20 04:05:49 -0700

    nnnnnnghhhh

---
+++ nnnhh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ls .
+git init
+git status
+git add -A
+git commit -m 'Initial commit.'
+git config --global user.name "David Nishikawa (but not really! don't keep this name. be sure to look into how to change it.. love u! bye)"
+git config --global user.email "not_nishikawa_david@gmail.com (but not really again, haha! don't keep this email. be sure to look into how to change it, too.. love u still bye!)"
+git status
+git commit -m 'Initial commit.'
+git log
+npm init
+
+#                       https://github.com/orgs/Feeling-Resonant-n-Shit/repositories/myFirstRepo.git
+# git remote add origin https://github.com/harishrajora805                          /myFirstRepo.git
+
+  git remote add origin https://github.com/orgs/Feeling-Resonant-n-Shit/repositories/myFirstRepo.git
+
+# /* nnnhh */
