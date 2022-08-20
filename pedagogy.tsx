c81242491dc07625027b7d73c3d747220177aa8a
David Nishikawa <nishikawadavid@gmail.com>
2022-08-20 03:21:51 -0700

    Initial commit.

---
+++ pedagogy.tsx
@@ -0,0 +1,53 @@
+
+
+const THE_CODE = `
+                                  of Art1Art of
+                                Pedagogy1Pedagogy
+                                       ]1[
+                         :mentoring When1When mentoring:
+                            explicit Be)1(Be explicit
+    listen to student the for space Make1Make space for the student to listen
+forward way own there into insight For <0> For insight into their own way forward
+        prepared yet are none if for And1And for if none are yet prepared
+                            pace Slowing1Slowing pace
+                        :steps Retracing1Retracing steps:
+                   [(foundation there Be1Be their foundation)]
+`
+
+const potentialFontUrls = [
+    { url: 'https://fonts.google.com/specimen/Montserrat                           ?                     preview.text=ACBabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Roboto+Mono                          ?                     preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Quicksand                            ?                     preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Josefin+Sans                         ?                     preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Lobster                              ?                     preview.text=ABCabc101&preview.text_type=custom',},
+    /* handwriting picks */
+    { url: 'https://fonts.google.com/specimen/Shadows+Into+Light                   ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Shadows+Into+Light                   ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Architects+Daughter                  ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Shadows+Into+Light+Two               ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Hi+Melody                            ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Schoolbell                           ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Fondamento                           ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Give+You+Glory                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Gaegu                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // FINALIZED ?
+    /* movin' along */
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+    // { url: 'https://fonts.google.com/specimen/Kalam                                ?category=Handwriting&preview.text=ABCabc101&preview.text_type=custom',},
+]
+
