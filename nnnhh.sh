37e7c474a8327e39135279954b779c7b20790988
David Nishikawa <nishikawadavid@gmail.com>
2022-08-20 05:24:35 -0700

    look at the scenery!

--- nnnhh.sh
+++ nnnhh.sh
@@ -110,13 +110,20 @@ chmod u+x nnnhh.sh
 
  git add -A
  git commit -m 'Welcome!'
- 1 file changed, 24 insertions(+)
- BugFix: oopsBugFix: : The term 'BugFix:' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is        correct and try again.At line:1 char:1+ BugFix: oops+ ~~~~~~~    + CategoryInfo          : ObjectNotFound: (BugFix::String) [], CommandNotFoundException    + FullyQualifiedErrorId : CommandNotFoundException
- git remote get-url    --push                query push URLs rather than fetch URLs    --all                 return all URLs
- git remote get-url originerror: No such remote 'origin'
- git remote set-url originusage: git remote set-url [--push] <name> <newurl> [<oldurl>]   or: git remote set-url --add <name> <newurl>   or: git remote set-url --delete <name> <url>    --push                manipulate push URLs    --add                 add URL    --delete              delete URLs
- git remote get-url originerror: No such remote 'origin'
+ git remote get-url    --push
+ git remote set-url 
+ git remote get-url
  git remote add origin https://github.com/Feeling-Resonant-n-Shit/the_code.git
  git logAuthor: David Nishikawa <nishikawadavid@gmail.com>    Welcome!commit bd33200495ec2c50cbdf2e814579bf8c1fbd5ce8Author: David Nishikawa <nishikawadavid@gmail.com>Date:   Sat Aug 20 04:49:49 2022 -0700    Love You!commit fb63da066d0c8e145f466d8e2f176564c0d0f712Author: David Nishikawa <nishikawadavid@gmail.com>Date:   Sat Aug 20 04:47:15 2022 -0700    pretty tired now..
  'q'
 'tiny key hhehe'
+
+git add commit -A 
+git commit -m 'breathe'
+
+'give yourself space'
+
+
+git add -A;git commit -m'look at the scenery!' ## and don't you worry hun, copypaste!
+# and don't you worry hun
+
